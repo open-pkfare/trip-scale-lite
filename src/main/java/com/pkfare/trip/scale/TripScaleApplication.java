a6d4249c7c216362bf6a7c55a95431c401aa7017
谢锦嵩 <carlos.xie@pkfare.com>
2025-08-18 10:14:22 +0800

    init

---
+++ src/main/java/com/pkfare/trip/scale/TripScaleApplication.java
@@ -0,0 +1,11 @@
+package com.pkfare.trip.scale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripScaleApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TripScaleApplication.class, args);
+    }
+}
