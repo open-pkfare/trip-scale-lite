200c59717568ef1207d03f0480e8b4ce14bc919e
谢锦嵩 <carlos.xie@pkfare.com>
2025-08-18 10:14:12 +0800

    init

--- src/main/java/com/pkfare/trip/scale/TripScaleController.java
+++ src/main/java/com/pkfare/trip/scale/TripScaleController.java
@@ -1,8 +1,14 @@
 package com.pkfare.trip.scale;
 
+import com.pkfare.trip.scale.dto.Conversation;
+import com.pkfare.trip.scale.dto.RespConversation;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class TripScaleController {
 
-
-
+    @GetMapping("/chat")
+    public RespConversation chat(Conversation conversation) {
+    }
 }
---
+++ src/main/java/com/pkfare/trip/scale/dto/RespConversation.java
@@ -0,0 +1,8 @@
+package com.pkfare.trip.scale.dto;
+
+import lombok.Data;
+
+@Data
+public class RespConversation extends Conversation {
+  private Object data;
+}
