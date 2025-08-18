2c6acc6e81930ffb70690e79549f6af8eba09c5c
谢锦嵩 <carlos.xie@pkfare.com>
2025-08-18 10:37:26 +0800

    init

--- src/main/java/com/pkfare/trip/scale/TripScaleController.java
+++ src/main/java/com/pkfare/trip/scale/TripScaleController.java
@@ -6,7 +6,7 @@ import com.pkfare.trip.scale.dto.RespConversation;
 import java.util.UUID;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,7 +15,7 @@ public class TripScaleController {
   @Autowired
   private RootAgent rootAgent;
 
-  @GetMapping("/chat")
+  @RequestMapping("/chat")
   public RespConversation chat(Conversation conversation) {
     if (StringUtils.isEmpty(conversation.getConversationId())) {
       conversation.setConversationId(UUID.randomUUID().toString());
