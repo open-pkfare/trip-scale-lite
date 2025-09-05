2668bc776901000016d12868c90b216223cbd944
刘洋 <louis.liu@pkfare.com>
2025-09-05 17:50:35 +0800

    back:planResult只返回地址经纬度

--- src/main/java/com/pkfare/trip/scale/plan/service/response/LocationPoint.java
+++ src/main/java/com/pkfare/trip/scale/plan/service/response/LocationPoint.java
@@ -1,17 +1,14 @@
 package com.pkfare.trip.scale.plan.service.response;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class LocationPoint {
 
   private String name;
   private double latitude;
   private double longitude;
 
-  public LocationPoint() {
-  }
-
-  public LocationPoint(String hotelName, double latitude, double longitude) {
-  }
 }
